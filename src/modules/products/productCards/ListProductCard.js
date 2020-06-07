0ce29c0b7fc39bc0e7987d38b66e88e09720f154
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-06-07 22:29:17 +0530

    setting up storybook with component description and prop table.

---
+++ src/modules/products/productCards/ListProductCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { getProductFields } from '../utils';
+
+const ListProductCard = ({ product, productMap, showVariants, productVariantMap }) => {
+
+    //Get the datas from the product bases on productMap and create the card
+    const productValues = getProductFields(product, productMap, showVariants, productVariantMap);
+
+    const {
+        productName,
+        imageUrl,
+        price,
+        sellingPrice } = productValues;
+
+    //Add support for router as a config
+    return (<div className='UNX-product-card-container' data-uniqueid={product.uniqueId}>
+        <a href={product.productUrl} className={`UNX-product-card UNX-list-card`} data-uniqueid={product.uniqueId}>
+            <img className='UNX-image' src={imageUrl} data-uniqueid={product.uniqueId} />
+            <p className='UNX-product-name data-uniqueid={product.uniqueId}'>{productName}</p>
+            <p className='UNX-price' data-uniqueid={product.uniqueId}>{price}</p>
+            <p className='UNX-selling-price' data-uniqueid={product.uniqueId}>{sellingPrice}</p>
+        </a>
+        {/* swatch content */}
+    </div>)
+}
+
+ListProductCard.propTypes = {
+    product: PropTypes.object.isRequired,
+    productMap: PropTypes.object.isRequired,
+    showVariants: PropTypes.bool.isRequired,
+    productVariantMap: PropTypes.object.isRequired
+}
+
+export default ListProductCard;
