5c3f1188ac93e949486f573e37787dc5a4746d95
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-06-26 17:49:00 +0530

    rebase fixes

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import List from './List';
 import Button from './Button';
 import Loader from './Loader';
+import Input from './Input';
+import RangeSlider from './RangeSlider';
 
-export { List, Input, Button, Loader };
+export { List, Input, Button, Loader, RangeSlider };
