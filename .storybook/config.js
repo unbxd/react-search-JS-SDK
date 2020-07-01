98baf491b3a9bc8a12026ceebaae985fbc55a125
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-07-01 14:00:41 +0530

    add storybook root stories sort

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,36 @@
 const { addDecorator } = require('@storybook/react');
 const { withPropsTable } = require('storybook-addon-react-docgen');
- 
+const { addParameters } = require('@storybook/react');
+
+
 addDecorator(withPropsTable);
+
+addParameters({
+    options: {
+        storySort: (a, b) => {
+
+            const rootStoriesOrder = {
+                Introduction: 1,
+                UnbxdSearchWrapper: 2,
+                Products: 3,
+                Facets: 4,
+                Pagination: 5,
+                Sort: 6,
+                SearchBox: 7,
+                SpellCheck: 8,
+                Banners: 9,
+            }
+
+            if (rootStoriesOrder[a[1].kind] < rootStoriesOrder[b[1].kind]) {
+                return -1;
+            }
+
+            if (rootStoriesOrder[a[1].kind] > rootStoriesOrder[b[1].kind]) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+    },
+});
