696fc7bced39f981d79a4432efaf115cbd4078a6
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-07-07 10:47:27 +0530

    setup build for demo.

---
+++ demo/webpack/webpack.dev.js
@@ -0,0 +1,80 @@
+const Path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: Path.resolve(__dirname, '../public/index.html'),
+    filename: "index.html"
+});
+
+module.exports = {
+    entry: Path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: Path.resolve(__dirname, '../public'),
+        publicPath: '/',
+        filename: 'js/[name].js',
+        sourceMapFilename: '[file].map'
+    },
+    mode: "development",
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            '@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties','transform-class-properties']
+                    }
+                }]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('tailwindcss'),
+                                require('autoprefixer'),
+                            ],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['public/css']
+                            },
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [htmlPlugin],
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            '~': Path.resolve(__dirname, '../src')
+        }
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        port: 6969
+    }
+};
---
+++ demo/webpack/webpack.prod.js
@@ -0,0 +1,80 @@
+const Path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: Path.resolve(__dirname, '../public/index.html'),
+    filename: "index.html"
+});
+
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+    path: Path.resolve(__dirname, '../public/dist'),
+    filename: 'css/[name].css',
+    chunkFilename: '[id].css'
+
+});
+
+const bundleAnalyzerPlugin = new BundleAnalyzerPlugin();
+
+module.exports = {
+    entry: Path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: Path.resolve(__dirname, '../public/dist'),
+        publicPath: '/',
+        filename: 'js/[name].js',
+        sourceMapFilename: '[file].map'
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            '@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['public/css']
+                            },
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+    plugins: [bundleAnalyzerPlugin, miniCssExtractPlugin, htmlPlugin],
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            '~': Path.resolve(__dirname, '../src')
+        }
+    }
+};
