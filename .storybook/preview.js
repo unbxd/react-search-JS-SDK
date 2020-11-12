f98fbeb6be0bf06d2dbe06ad50258357ae211e97
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-11-12 14:37:56 +0530

    feat: add facet actions to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,36 +5,37 @@ const { addParameters } = require('@storybook/react');
 addDecorator(withPropsTable);
 
 addParameters({
-  options: {
-    storySort: (a, b) => {
-      const rootStoriesOrder = {
-        Introduction: 1,
-        UnbxdSearchWrapper: 2,
-        Products: 3,
-        TextFacets: 4,
-        RangeFacets: 5,
-        MultilevelFacets: 6,
-        Breadcrumbs: 7,
-        SelectedFacets: 8,
-        SearchBox: 9,
-        PageSize: 10,
-        Pagination: 11,
-        Sort: 12,
-        SpellCheck: 13,
-        ViewTypes: 14,
-        Banners: 15,
-        SearchTitle: 16
-      };
+    options: {
+        storySort: (a, b) => {
+            const rootStoriesOrder = {
+                Introduction: 1,
+                UnbxdSearchWrapper: 2,
+                Products: 3,
+                TextFacets: 4,
+                RangeFacets: 5,
+                MultilevelFacets: 6,
+                Breadcrumbs: 7,
+                SelectedFacets: 8,
+                FacetActions: 9,
+                SearchBox: 10,
+                PageSize: 11,
+                Pagination: 12,
+                Sort: 13,
+                SpellCheck: 14,
+                ViewTypes: 15,
+                Banners: 16,
+                SearchTitle: 17,
+            };
 
-      if (rootStoriesOrder[a[1].kind] < rootStoriesOrder[b[1].kind]) {
-        return -1;
-      }
+            if (rootStoriesOrder[a[1].kind] < rootStoriesOrder[b[1].kind]) {
+                return -1;
+            }
 
-      if (rootStoriesOrder[a[1].kind] > rootStoriesOrder[b[1].kind]) {
-        return 1;
-      }
+            if (rootStoriesOrder[a[1].kind] > rootStoriesOrder[b[1].kind]) {
+                return 1;
+            }
 
-      return 0;
-    }
-  }
+            return 0;
+        },
+    },
 });
