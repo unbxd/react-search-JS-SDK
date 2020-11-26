c328f1bb82959553b09faf43b168803426fcefcc
Vikram Gupta <vikram.gupta@unbxd.com>
2020-11-26 12:45:12 +0530

    Test framework setup.

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // The root of your source code, typically /src
+    // `<rootDir>` is a token Jest substitutes
+    roots: ["<rootDir>/src"],
+
+    // Jest transformations -- this adds support for TypeScript
+    // using ts-jest
+    transform: {
+        '\\.(js|jsx)?$': 'babel-jest',
+    },
+
+    // Test spec file resolution pattern
+    // Matches parent folder `__tests__` and filename
+    // should contain `test` or `spec`.
+    // testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.js?$",
+
+    // Module file extensions for importing
+    moduleFileExtensions: ["js", "json"],
+    moduleNameMapper: {
+        "\\.(css|less|scss)$": "identity-obj-proxy"
+    },
+    setupFilesAfterEnv: [
+        '@testing-library/jest-dom/extend-expect',
+    ] // setupFiles before the tests are ran
+};
