420625adcb7d488171ce5a44fbb61478355887b1
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-11-27 15:45:47 +0530

    chore: test setup.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     // The root of your source code, typically /src
     // `<rootDir>` is a token Jest substitutes
     roots: ["<rootDir>/src"],
-
+    setupFiles: ["./setup.js"],
     // Jest transformations -- this adds support for TypeScript
     // using ts-jest
     transform: {
