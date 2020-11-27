510dcb98a7490c6ad907525b3702738ff1221529
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-11-27 15:45:47 +0530

    chore: test setup.

--- __mocks__/unbxd-search-core.js
+++
@@ -1,9 +0,0 @@
-const UnbxdSearch = jest.genMockFromModule('@unbxd-ui/unbxd-search-core');
-const getSearchResults = jest.fn(function () {
-    console.log('Being called!!!!')
-    return 'hello';
-})
-console.log('Reachedddd');
-UnbxdSearch.getSearchResults = getSearchResults;
-
-export default UnbxdSearch;
