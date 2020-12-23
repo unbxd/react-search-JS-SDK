fdfcd9535e1164b518c1c954251c6e32721f849d
sanjithkumar017 <sanjithkumar017@gmail.com>
2020-12-23 11:16:58 +0530

    fix(test): test

--- src/modules/analytics/analytics.js
+++ src/modules/analytics/analytics.js
@@ -21,7 +21,7 @@ export const trackCategory = function (unbxdAnalyticsConf) {
 };
 
 export const trackProductClick = function (uniqueId, prank) {
-    console.log("test")
+    console.log("test1")
     try {
         Unbxd.track('click', {
             pid: uniqueId,
