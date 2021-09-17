84d7270f15a7ea537e0875a7e90c87a2e780f5fa
sanjithkumar017 <sanjithkumar017@gmail.com>
2021-09-17 11:41:03 +0530

    fix(misc): misc updates

--- src/modules/products/ProductsContainer.js
+++ src/modules/products/ProductsContainer.js
@@ -57,7 +57,7 @@ class ProductsContainer extends React.PureComponent {
             productItemComponent,
             productIdAttribute,
             attributesMap = {},
-            showVariants,
+            showVariants = false,
             groupBy,
             variantAttributesMap = {},
             showSwatches,
